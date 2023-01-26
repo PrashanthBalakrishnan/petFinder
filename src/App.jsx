8e602dcf3aa37a4b7d92000fa56164407c81968f
PrashanthBalakrishnan <prashbala13@gmail.com>
2023-01-26 18:11:24 -0500

    petfinder site

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AdoptedPetContext from "./AdoptedPetContext";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,19 +19,39 @@ const queryClient = new QueryClient({
 const App = () => {
   const adoptedPet = useState(null);
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
+    <div
+      className="m-0 p-0"
+      style={{
+        background: "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
+      }}
+    >
+      <BrowserRouter>
         <AdoptedPetContext.Provider value={adoptedPet}>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
+          <QueryClientProvider client={queryClient}>
+            <Suspense
+              fallback={
+                <div className="loading-pane">
+                  <h2 className="loader">🐕</h2>
+                </div>
+              }
+            >
+              <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
+                <Link
+                  className="text-6xl text-white hover:text-gray-200"
+                  to="/"
+                >
+                  Adopt Me!
+                </Link>
+              </header>
+              <Routes>
+                <Route path="/details/:id" element={<Details />} />
+                <Route path="/" element={<SearchParams />} />
+              </Routes>
+            </Suspense>
+          </QueryClientProvider>
         </AdoptedPetContext.Provider>
-      </QueryClientProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </div>
   );
 };
 
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,43 +1,42 @@
-import { useState, useContext } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import AdoptedPetContext from "./AdoptedPetContext";
+import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import { useContext, useState } from "react";
+import AdoptedPetContext from "./AdoptedPetContext";
+import Modal from "./Modal";
+import ErrorBoundary from "./ErrorBoundary";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
-import ErrorBoundary from "./ErrorBoundary";
-import Modal from "./Modal";
 
 const Details = () => {
+  const { id } = useParams();
   const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
-  const [_, setAdoptedPet] = useContext(AdoptedPetContext);
-  const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
+  // eslint-disable-next-line no-unused-vars
+  const [_, setAdoptedPet] = useContext(AdoptedPetContext);
 
   if (results.isLoading) {
     return (
       <div className="loading-pane">
-        <h2 className="loader">☀️</h2>
+        <h2 className="loader">🌀</h2>
       </div>
     );
   }
 
   const pet = results.data.pets[0];
+
   return (
     <div className="details">
       <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
-        <h2>
-          {pet.animal} - {pet.breed} - {pet.city} - {pet.state}
-          <button onClick={() => setShowModal(true)}>Adobt{pet.name}</button>
-          <p>{pet.description}</p>
-          {showModal ? (
-            <Modal>
+        <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+        {showModal ? (
+          <Modal>
+            <div>
               <h1>Would you like to adopt {pet.name}?</h1>
-              <p className="small-text">
-                You are only allowed to adopt one pet at a time
-              </p>
               <div className="buttons">
                 <button
                   onClick={() => {
@@ -49,19 +48,18 @@ const Details = () => {
                 </button>
                 <button onClick={() => setShowModal(false)}>No</button>
               </div>
-            </Modal>
-          ) : null}
-        </h2>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     </div>
   );
 };
 
-function DetailsErrorBoundary() {
+export default function DetailsErrorBoundary(props) {
   return (
     <ErrorBoundary>
-      <Details />
+      <Details {...props} />
     </ErrorBoundary>
   );
 }
-export default DetailsErrorBoundary;
