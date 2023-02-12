edba52886739cda46763ba14834893b1607c479e
PrashanthBalakrishnan <prashbala13@gmail.com>
2023-02-12 00:56:49 -0500

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,12 @@
-import { useState, lazy, Suspense } from "react";
-import { createRoot } from "react-dom/client";
-import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import AdoptedPetContext from "./AdoptedPetContext";
+import SearchParams from "./components/SearchParams";
 
-const Details = lazy(() => import("./Details"));
-const SearchParams = lazy(() => import("./SearchParams"));
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: Infinity,
-      cacheTime: Infinity,
-    },
-  },
-});
-
-const App = () => {
-  const adoptedPet = useState(null);
+function App() {
   return (
-    <div
-      className="m-0 p-0"
-      style={{
-        background: "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
-      }}
-    >
-      <BrowserRouter>
-        <AdoptedPetContext.Provider value={adoptedPet}>
-          <QueryClientProvider client={queryClient}>
-            <Suspense
-              fallback={
-                <div className="loading-pane">
-                  <h2 className="loader">🐕</h2>
-                </div>
-              }
-            >
-              <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
-                <Link
-                  className="text-6xl text-white hover:text-gray-200"
-                  to="/"
-                >
-                  Adopt Me!
-                </Link>
-              </header>
-              <Routes>
-                <Route path="/details/:id" element={<Details />} />
-                <Route path="/" element={<SearchParams />} />
-              </Routes>
-            </Suspense>
-          </QueryClientProvider>
-        </AdoptedPetContext.Provider>
-      </BrowserRouter>
+    <div className="App">
+      <h1>Adopt Me!</h1>
+      <SearchParams />
     </div>
   );
-};
+}
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+export default App;
